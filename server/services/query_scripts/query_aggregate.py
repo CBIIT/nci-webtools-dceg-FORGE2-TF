9da3b9597c982cac64128e54de96673605529fef
kvnjng <kvvnjng@gmail.com>
2021-06-08 17:08:10 -0500

    fix export aws creds syntax

--- server/services/query_scripts/query_aggregate.py
+++ server/services/query_scripts/query_aggregate.py
@@ -99,12 +99,12 @@ def checkS3File(bucket, filePath):
     return True
 
 if ('aws_access_key_id' in aws_info and len(aws_info['aws_access_key_id']) > 0 and 'aws_secret_access_key' in aws_info and len(aws_info['aws_secret_access_key']) > 0):
-  export_s3_keys = "export AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s;" % (aws_info['aws_access_key_id'], aws_info['aws_secret_access_key'])
+  export_s3_keys = "export AWS_ACCESS_KEY_ID=%s; export AWS_SECRET_ACCESS_KEY=%s;" % (aws_info['aws_access_key_id'], aws_info['aws_secret_access_key'])
 else:
   # retrieve aws credentials here
   session = boto3.Session()
   credentials = session.get_credentials().get_frozen_credentials()
-  export_s3_keys = "export AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s;" % (credentials.access_key, credentials.secret_key)
+  export_s3_keys = "export AWS_ACCESS_KEY_ID=%s; export AWS_SECRET_ACCESS_KEY=%s;" % (credentials.access_key, credentials.secret_key)
 
 def tabix_call(cmd):
   try:
