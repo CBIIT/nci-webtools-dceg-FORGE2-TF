6b62ae04eca0ffd424426dafbd34900a02c6eb89
Phyllip Cho <phyllip3@users.noreply.github.com>
2025-06-06 11:18:07 -0400

    add express-validator to api routes

---
+++ server/services/apiValidation.js
@@ -0,0 +1,60 @@
+const { body } = require("express-validator");
+
+// available arrays in /data
+const availableArrays = ["All"];
+
+const queryProbeNamesValidation = [
+  body("settings.array").isString().isIn(availableArrays).trim().escape(),
+];
+
+const queryAggregateValidation = [
+  body("settings.currentProbe")
+    .isString()
+    .matches(/^rs\d+$/)
+    .trim()
+    .escape(),
+  body("settings.array").isString().isIn(availableArrays).trim().escape(),
+  body("settings.sample")
+    .isString()
+    .matches(/^[A-Za-z0-9_]+-[A-Za-z0-9_]+$/)
+    .trim()
+    .escape(),
+];
+
+const queryTfSummaryValidation = [
+  body("tf_summary.array").isString().isIn(availableArrays).trim().escape(),
+];
+
+const queryTfSummaryGraphValidation = [
+  body("tf_summary.output")
+    .isString()
+    .matches(/^[A-Za-z0-9_\-]+\.pdf$/) // Only allow filenames like "output-123.pdf"
+    .withMessage("Invalid output filename"),
+];
+
+const queryTfAggregateSummaryValidation = [
+  body("tf_aggregate_summary.array")
+    .isString()
+    .isIn(availableArrays)
+    .trim()
+    .escape(),
+  body("tf_aggregate_summary.sample")
+    .isString()
+    .matches(/^[A-Za-z0-9_.\-]+$/)
+    .withMessage("Invalid TF model name")
+    .trim()
+    .escape(),
+];
+
+const queryTfProbeOverlapSummaryValidation = [
+  body("tf_probe_overlap_summary.tfModel").isString().trim().escape(),
+];
+
+module.exports = {
+  queryProbeNamesValidation,
+  queryAggregateValidation,
+  queryTfSummaryValidation,
+  queryTfSummaryGraphValidation,
+  queryTfAggregateSummaryValidation,
+  queryTfProbeOverlapSummaryValidation,
+};
